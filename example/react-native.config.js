da5a4e9d6cf7394891736d2d7ef1ee69c3786c0e
Janic Duplessis <janicduplessis@gmail.com>
2026-01-02 01:35:09 -0500

    fix: android build

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const pkg = require('../package.json');
-const workletsPackage = require('../node_modules/react-native-worklets/package.json');
 
 module.exports = {
   project: {
@@ -11,9 +10,6 @@ module.exports = {
   dependencies: {
     [pkg.name]: {
       root: path.join(__dirname, '..'),
-    },
-    [workletsPackage.name]: {
-      root: path.join(__dirname, '../node_modules/react-native-worklets'),
     },
   },
 };
